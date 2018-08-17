b0a3fe2a026d9c9b78f58360b29b7064cea34d31
WhimsicalOne <dean@local-design.co.za>
2018-08-17 11:41:58 +0200

    Additions

---
+++ js/main.js
@@ -0,0 +1,11 @@
+// Write functions
+const changeToBlack = () => {
+    $('body').css('background', 'black');
+}
+
+
+
+$(document).ready(function() {
+    // call functions here...
+    changeToBlack();
+});
